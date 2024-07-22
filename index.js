4035fc38e9494a8dc11e7a38bc59175ba187becb
ShubhamGavand1234 <shubhamgavand@yahoo.in>
2024-07-22 21:50:40 +0530

    22/07/2024

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import axios from "axios";
 
 const app = express();
 const port = 3000;
+const key = "AIzaSyB87tkJzQ-waEaIdIK9YLh4Tk8EFY18iE0";
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -19,18 +20,64 @@ const db = new pg.Client({
 });
 
 db.connect();
-app.get("/", async (req, res) => {
-  const result = await db.query(
-    "select b.id, b.title, b.author, d.description from books as b join details as d ON b.id = d.book_id;"
-  );
-  const bookData = result.rows;
-  console.log(result.rows);
 
-  res.render("index.ejs", { books: bookData });
+let logedInUser = 0;
+
+app.get("/", async (req, res) => {
+  if (logedInUser !== 0) {
+    try {
+      // const result = await db.query(
+      //   "select b.id, b.title, b.author, d.description from books as b join details as d ON b.id = d.book_id;"
+      // );
+      const result = await db.query(
+        "select b.id, b.title, b.author, r.review from reviews as r Join users as u ON user_id = u.id JOIN books as b ON book_id = b.id where  u.id = $1;",
+        [logedInUser]
+      );
+      const bookData = result.rows;
+      console.log(result.rows);
+
+      res.render("index.ejs", { books: bookData });
+    } catch (error) {}
+  } else {
+    res.send("<h1></h1> <a href='/login'>Login</a>");
+  }
+});
+
+app.get("/findbook", async (req, res) => {
+  try {
+    res.render("findbook.ejs");
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.post("/findbook", async (req, res) => {
+  try {
+    const book = req.body.book;
+    console.log(book);
+    const response = await axios.get(
+      `https://www.googleapis.com/books/v1/volumes?q='${book}'&maxResults=40&key=${key}`
+    );
+    const googlebooks = [];
+    let id = 1;
+    response.data.items.forEach((book) => {
+      let newBook = {
+        id: id++,
+        title: book.volumeInfo.title,
+        author: book.volumeInfo.authors[0] || "Unknown",
+        description: book.volumeInfo.description
+          ? book.volumeInfo.description
+          : "No description available",
+      };
+      googlebooks.push(newBook);
+    });
+    res.render("googlebooks.ejs", { books: googlebooks });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 app.get("/googlebooks", async (req, res) => {
-  const key = "AIzaSyB87tkJzQ-waEaIdIK9YLh4Tk8EFY18iE0";
   try {
     const response = await axios.get(
       `https://www.googleapis.com/books/v1/volumes?q=pokemon&maxResults=40&key=${key}`
@@ -55,10 +102,58 @@ app.get("/googlebooks", async (req, res) => {
     });
     //   console.log(googlebooks.length, googlebooks);
     //   res.sendStatus(200);
-    res.render("index.ejs", { books: googlebooks });
+    res.render("googlebooks.ejs", { books: googlebooks });
   } catch (error) {
     console.log(error);
     res.status(500).send("Internal Server error");
+  }
+});
+
+//registraation get
+app.get("/registration", async (req, res) => {
+  try {
+    res.render("registrationForm.ejs");
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.post("/registration", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    console.log(username, password);
+    await db.query("insert into users (username, password) values ($1 , $2);", [
+      username,
+      password,
+    ]);
+    res.redirect("/login");
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+//login routes
+app.get("/login", async (req, res) => {
+  try {
+    res.render("login.ejs");
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    console.log(username, password);
+    const result = await db.query(
+      "select id from users where username = $1 and password = $2;",
+      [username, password]
+    );
+    console.log(result.rows[0].id);
+    logedInUser = result.rows[0].id;
+    res.redirect("/");
+  } catch (error) {
+    console.log(error);
   }
 });
 
